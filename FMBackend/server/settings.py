eae8cef92b4331c6ea46ae2280b44461a52d3b17
indreshp135 <indreshp135@gmail.com>
2022-07-07 15:03:17 +0530

    fix: workflows

--- FMBackend/server/settings.py
+++ FMBackend/server/settings.py
@@ -176,8 +176,4 @@ SITE_ID = int(os.environ.get("SITE_ID"))
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = "/uploads/"
 
-<<<<<<< HEAD
-CSRF_TRUSTED_ORIGINS = ["https://fm.indreshp.xyz"]
-=======
-CSRF_TRUSTED_ORIGINS = [os.environ.get("HOSTED_URL")]
->>>>>>> feat: multiple flow (#21)
+CSRF_TRUSTED_ORIGINS = os.environ.get("HOSTED_URL").split(" ")
